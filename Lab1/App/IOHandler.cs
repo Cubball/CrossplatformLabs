0c8208fffc796934bec2a7742555cbf9693024d1
Cubball <138378823+Cubball@users.noreply.github.com>
2024-09-14 07:38:48 +0300

    feat(lab1): error handling

--- Lab1/App/IOHandler.cs
+++ Lab1/App/IOHandler.cs
@@ -9,36 +9,39 @@ public static class IOHandler
     {
         if (!File.Exists(InputFileName))
         {
-            throw new Exception($"Файл {InputFileName} не було знайдено.");
+            throw new InputException($"Файл {InputFileName} не було знайдено.");
         }
 
-        var lines = File.ReadAllLines(InputFileName).Select(line => line.Trim())
-            .Where(line => !string.IsNullOrWhiteSpace(line))
+        var lines = File.ReadAllLines(InputFileName)
+            .Select(static line => line.Trim())
+            .Where(static line => !string.IsNullOrWhiteSpace(line))
             .ToArray();
         if (lines.Length == 0)
         {
-            throw new Exception("Файл не містить жодного тексту");
+            throw new InputException("Файл не містить жодного тексту");
         }
 
         if (lines.Length != 1)
         {
-            throw new Exception("Файл містить більше одного рядка з даними");
+            throw new InputException("Файл містить більше одного рядка з даними");
         }
 
-        var parts = lines[0].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var parts = lines[0]
+            .Trim()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries);
         if (parts.Length != 2)
         {
-            throw new Exception("Рядок має містити два числа, розділені пробілом");
+            throw new InputException("Рядок має містити два числа, розділені пробілом");
         }
 
         if (!int.TryParse(parts[0], out var sum))
         {
-            throw new Exception("Перше число має бути цілим числом");
+            throw new InputException("Перше число має бути цілим числом");
         }
 
         if (!int.TryParse(parts[1], out var digitsCount))
         {
-            throw new Exception("Друге число має бути цілим числом");
+            throw new InputException("Друге число має бути цілим числом");
         }
 
         return (sum, digitsCount);
--- Lab1/App/Program.cs
+++ Lab1/App/Program.cs
@@ -1,9 +1,38 @@
-﻿using App;
-using System.Text;
+﻿using System.Text;
+using App;
 
-Console.OutputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.Unicode;
 
-var (sum, digitsCount) = IOHandler.ReadNumbersFromFile();
-var max = NumberService.FindMaxForSumAndDigitsCount(sum, digitsCount);
-var min = NumberService.FindMinForSumAndDigitsCount(sum, digitsCount);
-IOHandler.WriteResultToFile(max, min);
+int sum;
+int digitsCount;
+try
+{
+    (sum, digitsCount) = IOHandler.ReadNumbersFromFile();
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Під час зчитування файлу виникла помилка: {e.Message}");
+    return;
+}
+
+string max;
+string min;
+try
+{
+    max = NumberService.FindMaxForSumAndDigitsCount(sum, digitsCount);
+    min = NumberService.FindMinForSumAndDigitsCount(sum, digitsCount);
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Знаходження розв'язку виникла помилка: {e.Message}");
+    return;
+}
+
+try
+{
+    IOHandler.WriteResultToFile(max, min);
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Під час запису файлу виникла помилка: {e.Message}");
+}
