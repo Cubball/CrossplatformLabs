bb5e9eb3b63fcb2a539ed37581eafb4fc87f6041
Cubball <138378823+Cubball@users.noreply.github.com>
2024-11-06 14:27:44 +0200

    fix(lab4): fix subcommand flags

--- Lab4/R_Davydchuk/Program.cs
+++ Lab4/R_Davydchuk/Program.cs
@@ -52,23 +52,22 @@ app.Command("run", run =>
         return 1;
     });
 
-    var input = run.Option("-i|--input", "Input file path", CommandOptionType.SingleValue);
-    var output = run.Option("-o|--output", "Output file path", CommandOptionType.SingleValue);
+    var input = run.Option("-i|--input", "Input file path", CommandOptionType.SingleValue, true);
+    var output = run.Option("-o|--output", "Output file path", CommandOptionType.SingleValue, true);
 
     run.Command("lab1", lab1 =>
     {
         lab1.Description = "Run lab 1";
-        var folderPath = Environment.GetEnvironmentVariable("LAB_PATH");
-        if (string.IsNullOrWhiteSpace(folderPath))
-        {
-            folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        }
-
-        var inputFilePath = input.HasValue() ? input.Value() : Path.Combine(folderPath, DefaultInputFileName);
-        var outputFilePath = output.HasValue() ? output.Value() : Path.Combine(folderPath, DefaultOutputFileName);
-
         lab1.OnExecute(() =>
         {
+            var folderPath = Environment.GetEnvironmentVariable("LAB_PATH");
+            if (string.IsNullOrWhiteSpace(folderPath))
+            {
+                folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            }
+
+            var inputFilePath = input.HasValue() ? input.Value() : Path.Combine(folderPath, DefaultInputFileName);
+            var outputFilePath = output.HasValue() ? output.Value() : Path.Combine(folderPath, DefaultOutputFileName);
             FirstLab.Execute(inputFilePath ?? "", outputFilePath ?? "");
             return 0;
         });
@@ -77,17 +76,16 @@ app.Command("run", run =>
     run.Command("lab2", lab2 =>
     {
         lab2.Description = "Run lab 2";
-        var folderPath = Environment.GetEnvironmentVariable("LAB_PATH");
-        if (string.IsNullOrWhiteSpace(folderPath))
-        {
-            folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        }
-
-        var inputFilePath = input.HasValue() ? input.Value() : Path.Combine(folderPath, DefaultInputFileName);
-        var outputFilePath = output.HasValue() ? output.Value() : Path.Combine(folderPath, DefaultOutputFileName);
-
         lab2.OnExecute(() =>
         {
+            var folderPath = Environment.GetEnvironmentVariable("LAB_PATH");
+            if (string.IsNullOrWhiteSpace(folderPath))
+            {
+                folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            }
+
+            var inputFilePath = input.HasValue() ? input.Value() : Path.Combine(folderPath, DefaultInputFileName);
+            var outputFilePath = output.HasValue() ? output.Value() : Path.Combine(folderPath, DefaultOutputFileName);
             SecondLab.Execute(inputFilePath ?? "", outputFilePath ?? "");
             return 0;
         });
@@ -96,17 +94,16 @@ app.Command("run", run =>
     run.Command("lab3", lab3 =>
     {
         lab3.Description = "Run lab 3";
-        var folderPath = Environment.GetEnvironmentVariable("LAB_PATH");
-        if (string.IsNullOrWhiteSpace(folderPath))
-        {
-            folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        }
-
-        var inputFilePath = input.HasValue() ? input.Value() : Path.Combine(folderPath, DefaultInputFileName);
-        var outputFilePath = output.HasValue() ? output.Value() : Path.Combine(folderPath, DefaultOutputFileName);
-
         lab3.OnExecute(() =>
         {
+            var folderPath = Environment.GetEnvironmentVariable("LAB_PATH");
+            if (string.IsNullOrWhiteSpace(folderPath))
+            {
+                folderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            }
+
+            var inputFilePath = input.HasValue() ? input.Value() : Path.Combine(folderPath, DefaultInputFileName);
+            var outputFilePath = output.HasValue() ? output.Value() : Path.Combine(folderPath, DefaultOutputFileName);
             ThirdLab.Execute(inputFilePath ?? "", outputFilePath ?? "");
             return 0;
         });
